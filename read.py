6837af547339666d469b6ab61bd1cb6e5e24cc71
jonasbarth <jonas.barth.95@gmail.com>
2023-06-16 21:07:06 +0200

    parallel vs sequential * make possible to either run sequential or parallel reading of pcap in read script

--- read.py
+++ read.py
@@ -1,5 +1,16 @@
-from reading import read_parallel
+import argparse
+
+from reading import read_parallel, read_sequential
 
 
 if __name__ == '__main__':
-    read_parallel('data/packets.pcap', 1000, 100)
+    parser = argparse.ArgumentParser(description='Reading files')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-p', '--parallel', action='store_true')
+    group.add_argument('-s', '--sequential', action='store_true')
+    args = parser.parse_args()
+
+    if args.parallel:
+        read_parallel('data/packets.pcap', 1000, 100)
+    elif args.sequential:
+        read_sequential('data/packets.pcap')
