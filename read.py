02399be3e0db07f48a986a8d8c0336e0b89f422b
jonasbarth <jonas.barth.95@gmail.com>
2023-06-17 11:30:16 +0200

    parallel vs sequential * add logging statements * time the reading functions * save times at the end

--- read.py
+++ read.py
@@ -1,16 +1,54 @@
 import argparse
+import os
+import timeit
+import logging
+
+import pandas as pd
 
 from reading import read_parallel, read_sequential
 
 
 if __name__ == '__main__':
+    logging.basicConfig(format='%(name)s:%(levelname)s:%(asctime)s - %(message)s', handlers=[
+        logging.FileHandler("debug.log", mode='a'),
+        logging.StreamHandler()
+    ])
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
     parser = argparse.ArgumentParser(description='Reading files')
+
+    parser.add_argument('-f', '--file', type=str, required=True, help="Path to the pcap file to be read.")
+    parser.add_argument('-o', '--out', type=str, required=True, help="Output directory where the timing data will be saved.")
+
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-p', '--parallel', action='store_true')
     group.add_argument('-s', '--sequential', action='store_true')
     args = parser.parse_args()
 
     if args.parallel:
-        read_parallel('data/packets.pcap', 1000, 100)
+        logger.info(f"Reading from {args.file} in parallel.")
+        read_type = 'parallel'
+        times = timeit.Timer(lambda: read_parallel(args.file, 1000, 100)).repeat(1, 1)
+        logger.info(f"Finished reading.")
+
     elif args.sequential:
-        read_sequential('data/packets.pcap')
+        logger.info(f"Reading from {args.file} sequentially.")
+        read_type = 'sequential'
+        times = timeit.Timer(lambda: read_sequential(args.file)).repeat(1, 1)
+
+        logger.info(f"Finished reading.")
+
+    out_path = f'{args.out}timing.feather'
+
+    timing_df = pd.DataFrame([[read_type, min(times)]], columns=['read_type', 'time_s'])
+
+    if os.path.exists(out_path):
+        logger.info(f"Found existing dataframe at {out_path}.")
+        df = pd.read_feather(out_path)
+
+        logger.info(f"Appending to existing dataframe.")
+        timing_df = pd.concat((timing_df, df), axis=0, ignore_index=True)
+
+    timing_df.to_feather(out_path)
+    logger.info(f"Saved dataframe to {out_path}.")
