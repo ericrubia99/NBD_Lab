d891617fdb3d58b10db86c76c7edfea4ac209561
jonasbarth <jonas.barth.95@gmail.com>
2023-07-06 21:53:28 +0200

    clean up * export point 5 plot into external package

--- plot/bit_rate.py
+++ plot/bit_rate.py
@@ -2,7 +2,7 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import plotly.express as px
 from util import bit_rate
 
 
@@ -23,3 +23,23 @@ def plot_bit_rate(top_ip_data: pd.DataFrame, ip_data: pd.DataFrame, ip: str):
     n = top_ip_data.shape[0]
     fig.suptitle(f'TOP {n} IP Dst for {ip}', fontsize=16)
     fig.show()
+
+
+def plot_total_bit_rate(data: pd.DataFrame):
+    bit_plot = px.line(data,
+                       x=[i * 0.1 for i in range(1, len(list(map(lambda x: x/1e6, bit_rate(data, 0.1)))) + 1)],
+                       y=list(map(lambda x: x/1e6, bit_rate(data, 0.1))),
+                       template='plotly_white',
+                       title='Total bit rate',
+                       markers=True)
+    bit_plot['data'][0]['showlegend'] = True
+    bit_plot['data'][0]['name'] = '0.1 sec'
+    bit_plot.add_scatter(x=[i * 0.4 for i in range(1, len(list(map(lambda x: x/1e6, bit_rate(data, 0.4)))) + 1)],
+                         y=list(map(lambda x: x/1e6, bit_rate(data, 0.4))), name="0.4 sec")
+    bit_plot.add_scatter(x=[i * 0.8 for i in range(1, len(list(map(lambda x: x/1e6, bit_rate(data, 0.8)))) + 1)],
+                         y=list(map(lambda x: x/1e6, bit_rate(data, 0.8))), name="0.8 sec")
+    bit_plot.update_layout(
+        yaxis_title_text ='Mbps',
+        xaxis_title_text ='Time(s)')
+
+    bit_plot.show()
